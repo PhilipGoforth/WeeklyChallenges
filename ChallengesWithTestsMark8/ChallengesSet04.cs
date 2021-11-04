9f376f63f6629642c1b78b84783e94fb0d491968
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-03 21:20:59 -0500

    on week 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,22 +8,38 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            foreach (var num in numbers)
+            {
+                if(num %2 == 0)
+                {
+                    result += num;
+                }
+                else
+                {
+                    result -= num;
+                }
+            }
+            return result;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var str = new string[] { str1, str2, str3, str4 };
+            int minLength = str.Min(y => y.Length);
+            return minLength;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var str = new int[] { number1, number2, number3, number4 };
+            int min = str.Min();
+            return min;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
@@ -41,7 +59,31 @@ namespace ChallengesWithTestsMark8
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            var Evens = new List<int>();
+            if (numbers != null)
+            {
+                foreach (var num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        Evens.Add(num);
+                    }
+                }
+                double average = Evens.Sum() / Evens.Count;
+                if(Evens.Count <= 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return average;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+            
         }
 
         public int Factorial(int number)
